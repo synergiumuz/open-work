1731efae301897982f57b19d133460f16fded908
Kkkiittt <khamidov357@gmail.com>
2023-02-20 16:47:57 +0500

    unit of work(only users)

--- src/OpenWork.DataAccess/Interfaces/IBasicRepository.cs
+++ src/OpenWork.DataAccess/Interfaces/IBasicRepository.cs
@@ -4,9 +4,9 @@ namespace OpenWork.DataAccess.Interfaces;
 
 public interface IBasicRepository<T> where T : BaseEntity
 {
-	public Task<bool> AddAsync(T entity);
-	public Task<bool> UpdateAsync(T entity);
+	public bool Add(T entity);
+	public bool Update(T entity);
 	public Task<bool> DeleteAsync(long id);
 	public Task<T> GetAsync(long id);
-	public IQueryable<T> GetALl();
+	public IQueryable<T> GetAll();
 }
---
+++ src/OpenWork.DataAccess/Repositories/BasicRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+
+using OpenWork.DataAccess.DbContexts;
+using OpenWork.DataAccess.Interfaces;
+using OpenWork.Domain.Common;
+
+namespace OpenWork.DataAccess.Repositories;
+
+public class BasicRepository<T> : IBasicRepository<T> where T : BaseEntity
+{
+	protected readonly DbSet<T> _set;
+
+	public BasicRepository(AppDbContext context)
+	{
+		_set = context.Set<T>();
+	}
+
+	public bool Add(T entity)
+	{
+		_set.Add(entity);
+		return true;
+	}
+
+	public async Task<bool> DeleteAsync(long id)
+	{
+		_set.Remove(await GetAsync(id));
+		return true;
+	}
+
+	public IQueryable<T> GetAll()
+	{
+		return _set.AsQueryable();
+	}
+
+	public async Task<T> GetAsync(long id)
+	{
+		var entity = await GetAsync(id);
+		return entity is null ? throw new Exception("Entity Not Found") : entity;
+	}
+
+	public bool Update(T entity)
+	{
+		_set.Update(entity);
+		return true;
+	}
+}

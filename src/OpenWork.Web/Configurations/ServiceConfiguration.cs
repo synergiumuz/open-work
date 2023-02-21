36ae006ba764364ed00d7b1347ee9658a761e17d
Shoxrux Husenov <husenovshoxrux71@gmail.com>
2023-02-21 18:15:45 +0500

    ppppppppppp

---
+++ src/OpenWork.Web/Configurations/ServiceConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Npgsql.Internal.TypeHandlers.DateTimeHandlers;
+using OpenWork.DataAccess.Interfaces;
+using OpenWork.DataAccess.Interfaces.Common;
+using OpenWork.DataAccess.Repositories;
+using OpenWork.DataAccess.Repositories.Common;
+using OpenWork.Services.Common.Security;
+using OpenWork.Services.Interfaces;
+using OpenWork.Services.Interfaces.Security;
+using OpenWork.Services.Services;
+using System.Runtime.CompilerServices;
+
+namespace OpenWork.Web.Configurations;
+
+public static class ServiceConfiguration
+{
+    public static void AddService(this IServiceCollection services)
+    {
+        services.AddHttpContextAccessor();
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+        services.AddScoped<IUserService, UserService>();
+        services.AddScoped<IHasher, Hasher>();
+        services.AddScoped<IAuthManager, AuthManager>();
+    }
+}

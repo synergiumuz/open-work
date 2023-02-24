11922e843746a7d0a0562ed97588b01b4653a79a
Sanjar Mavlonov <sanjarmavlonov9@gmail.com>
2023-02-24 12:00:19 +0500

    Added Admin panel

---
+++ src/OpenWork.Web/Areas/Administrator/Controllers/AccountsController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OpenWork.Services.Dtos;
+using OpenWork.Services.Interfaces;
+
+namespace OpenWork.Web.Areas.Administrator.Controllers;
+
+public class AccountsController : BaseController
+{
+	
+}
---
+++ src/OpenWork.Web/Areas/Administrator/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OpenWork.Web.Areas.Administrator.Controllers;
+
+public class HomeController : BaseController
+{
+	public IActionResult Index()
+	{
+		return View();
+	}
+}

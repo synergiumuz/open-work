a1bd83ebef3ec06e2b78ca28c8617faec7d3e538
Kkkiittt <khamidov357@gmail.com>
2023-03-04 19:34:17 +0500

    busyness service

--- src/OpenWork.DataAccess/Interfaces/Common/IBusynessRepository.cs
+++ src/OpenWork.DataAccess/Interfaces/Common/IBusynessRepository.cs
@@ -2,7 +2,7 @@
 
 namespace OpenWork.DataAccess.Interfaces.Common;
 
-public interface IBusinessRepository : IBasicRepository<Busyness>
+public interface IBusynessRepository : IBasicRepository<Busyness>
 {
 
 }
--- src/OpenWork.Services/Services/BusynessService.cs
+++ src/OpenWork.Services/Services/BusynessService.cs
@@ -4,10 +4,56 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using OpenWork.DataAccess.Interfaces;
+using OpenWork.Domain.Entities;
+using OpenWork.Services.Common.Utils;
+using OpenWork.Services.Dtos.Workers;
 using OpenWork.Services.Interfaces;
+using OpenWork.Services.Interfaces.Common;
 
 namespace OpenWork.Services.Services;
 
 public class BusynessService : IBusynessService
 {
+	private readonly IUnitOfWork _repository;
+	private readonly IIdentityService _identity;
+	private readonly IPaginatorService _paginator;
+	private readonly int _pageSize = 20;
+
+	public BusynessService(IUnitOfWork repository, IIdentityService identity, IPaginatorService paginator)
+	{
+		_repository = repository;
+		_identity = identity;
+		_paginator = paginator;
+	}
+
+	public async Task<bool> CreateAsync(BusynessCreateDto dto)
+	{
+		Busyness entity = new()
+		{
+			Start = dto.Start,
+			End = dto.End,
+			WorkerId = _identity.Id
+		};
+		_repository.Busynesses.Add(entity);
+		return await _repository.SaveChangesAsync() > 0;
+	}
+
+	public async Task<bool> DeleteAsync(long id)
+	{
+		await _repository.Busynesses.DeleteAsync(id);
+		return await _repository.SaveChangesAsync() > 0;
+	}
+
+	public Task<IEnumerable<Busyness>> GetAllAsync(int page)
+	{
+		return _paginator.PaginateAsync(_repository.Busynesses.GetAll(), new PaginationParams(_pageSize, page));
+	}
+
+	public Task<IEnumerable<Busyness>> SearchAsync(BusynessSearchDto dto, int page)
+	{
+		return _paginator.PaginateAsync(_repository.Busynesses.GetAll()
+			.Where(x => DateOnly.FromDateTime(x.Start) > dto.FromDate && DateOnly.FromDateTime(x.End) < dto.FromDate && TimeOnly.FromDateTime(x.Start) > dto.FromTime && TimeOnly.FromDateTime(x.End) < dto.ToTime)
+			, new PaginationParams(_pageSize, page));
+	}
 }

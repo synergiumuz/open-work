9cf42990072db0d24ecb1b1a81eadce9ec102f50
Kkkiittt <khamidov357@gmail.com>
2023-03-06 14:58:53 +0500

    service fixes

--- src/OpenWork.Services/Interfaces/ISkillService.cs
+++ src/OpenWork.Services/Interfaces/ISkillService.cs
@@ -1,9 +1,14 @@
 ﻿using System.Threading.Tasks;
 
+using OpenWork.Services.Dtos.Admins;
+
 namespace OpenWork.Services.Interfaces;
 
 public interface ISkillService
 {
 	public Task<bool> AddAsync(long skillId);
 	public Task<bool> RemoveAsync(long skillId);
+	public Task<bool> CreateAsync(SkillCreateDto dto);
+	public Task<bool> UpdateAsync(SkillCreateDto dto, long id);
+	public Task<bool> DeleteAsync(long id);
 }
--- src/OpenWork.Services/Services/SkillService.cs
+++ src/OpenWork.Services/Services/SkillService.cs
@@ -2,6 +2,7 @@
 
 using OpenWork.DataAccess.Interfaces;
 using OpenWork.Domain.Entities;
+using OpenWork.Services.Dtos.Admins;
 using OpenWork.Services.Interfaces;
 using OpenWork.Services.Interfaces.Common;
 
@@ -25,10 +26,38 @@ public class SkillService : ISkillService
 		return await _repository.SaveChangesAsync() > 0;
 	}
 
+	public async Task<bool> CreateAsync(SkillCreateDto dto)
+	{
+		Skill entity = new()
+		{
+			CategoryId = dto.CategoryId,
+			Description = dto.Description,
+			Name = dto.Name,
+		};
+		_repository.Skills.Add(entity);
+		return await _repository.SaveChangesAsync() > 0;
+	}
+
+	public async Task<bool> DeleteAsync(long id)
+	{
+		await _repository.Skills.DeleteAsync(id);
+		return await _repository.SaveChangesAsync() > 0;
+	}
+
 	public async Task<bool> RemoveAsync(long skillId)
 	{
 		Worker entity = await _repository.Workers.GetAsync(_identity.Id);
 		_ = entity.Skills.Remove(await _repository.Skills.GetAsync(skillId));
+		return await _repository.SaveChangesAsync() > 0;
+	}
+
+	public async Task<bool> UpdateAsync(SkillCreateDto dto, long id)
+	{
+		Skill entity = await _repository.Skills.GetAsync(id);
+		entity.Name = dto.Name;
+		entity.Description = dto.Description;
+		entity.CategoryId = dto.CategoryId;
+		_repository.Skills.Update(entity);
 		return await _repository.SaveChangesAsync() > 0;
 	}
 }

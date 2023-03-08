08f865ef3cca867d3d9dc7ac99e2678d6d1bb08f
Kkkiittt <khamidov357@gmail.com>
2023-03-08 17:03:42 +0500

    integration tests for user-cycle

--- test/OpenWork.IntegrationTest/UserTests.cs
+++ test/OpenWork.IntegrationTest/UserTests.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc.Testing;
+﻿using System.Net.Http.Json;
+
+using Microsoft.AspNetCore.Mvc.Testing;
 
 using OpenWork.IntegrationTest.Customs;
 using OpenWork.Services.Dtos.Users;
@@ -36,5 +38,42 @@ public class UserTests : IClassFixture<CustomWebApplicationFactory<Program>>
 		};
 		var response = await _client.PostAsync("/users/login", httpContent);
 		Assert.True(response.IsSuccessStatusCode, await response.Content.ReadAsStringAsync());
+	}
+
+	[Theory]
+	[InlineData("Xamidov", "Komiljon", "some@mail.com", "SomeLong357++")]
+	[InlineData("Sobirjonov", "O'tkirbek", "different@tiut.uz", "AAaa@@11")]
+	[InlineData("Po'latov", "Temurbek", "temur40@mail.ru", "N0tl0ngP@ssword")]
+	public async Task UserCycle_ReturnsTrue(string name, string surname, string email, string password)
+	{
+		try
+		{
+			UserRegisterDto registerDto = new UserRegisterDto()
+			{
+				Name = name,
+				Surname = surname,
+				Email = email,
+				Password = password
+			};
+			UserLoginDto loginDto = new UserLoginDto()
+			{
+				Email = email,
+				Password = password
+			};
+			var registerResponse = await _client.PostAsJsonAsync("/users/register", registerDto);
+			if(!registerResponse.IsSuccessStatusCode)
+				Assert.Fail(await registerResponse.Content.ReadAsStringAsync() + "\n" + await registerResponse.RequestMessage.Content.ReadAsStringAsync());
+			var loginResponse = await _client.PostAsJsonAsync("/users/login", loginDto);
+			if(!loginResponse.IsSuccessStatusCode)
+				Assert.Fail(await loginResponse.Content.ReadAsStringAsync());
+			_client.DefaultRequestHeaders.Add("Authorization", $"Bearer {await loginResponse.Content.ReadAsStringAsync()}");
+			var deleteResponse = await _client.DeleteAsync("/users/delete");
+			if(!deleteResponse.IsSuccessStatusCode)
+				Assert.Fail(await deleteResponse.Content.ReadAsStringAsync());
+		}
+		catch(Exception ex)
+		{
+			Assert.Fail(ex.Message);
+		}
 	}
 }
